d70350cfcb9437057d6435a92c201604fb66c7af
efode <efidesalew@gmail.com>
2019-11-27 11:05:02 -0500

    updating config manager

--- ConfigManager/ConfigManager.py
+++ ConfigManager/ConfigManager.py
@@ -15,3 +15,32 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+import configparser
+
+
+def write_config(config_file_path, section, sub_section, value):
+    """
+
+    :param config_file_path:
+    :param section:
+    :param sub_section:
+    :param value:
+    """
+    config = configparser.ConfigParser()
+    config[section] = {}
+    config[section][sub_section] = str(value)
+    with open(config_file_path, 'w') as configfile:
+        config.write(configfile)
+
+
+def read_config(config_file_path, section, sub_section):
+    """
+
+    :param config_file_path:
+    :param section:
+    :param sub_section:
+    :return:
+    """
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    return config[section][sub_section]
