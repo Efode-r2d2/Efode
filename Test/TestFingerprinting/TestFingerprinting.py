9b20befb202295b6f6b845e4a0ffa8a568504197
efode <efidesalew@gmail.com>
2019-11-28 08:35:05 -0500

    test audio fingerprinting

--- Test/TestFingerprinting/TestFingerprinting.py
+++ Test/TestFingerprinting/TestFingerprinting.py
@@ -31,9 +31,51 @@ src_dir = "../../../Test_Data/Reference_Audios"
 r_tree_path = "../../../Hashes/Efode/R_Tree"
 # raw_data_path
 raw_data_path = "../../../Raw_Data/Efode/Raw_Data"
+# config file path
+config_file_path = "../../Config/Config.ini"
 # spectrogram, peak  extractor and fingerprint generator objects
 stft = Spectrogram(hop_length=32)
 peak_extractor = PeakExtractor()
 fingerprint_generator = FingerprintGenerator()
 # searching for all .mp3 files under specified source dir
 mp3_files = DirManager.find_mp3_files(src_dir=src_dir)
+# get r_tree_index
+r_tree_index = RTreeManager.get_rtree_index(rtree_path=r_tree_path)
+# shelf index
+shelf_index = RawDataManager.get_shelf_file_index(shelf_path=raw_data_path)
+# fingerprinting files
+for i in mp3_files[0:5]:
+    # audio fingerprints
+    audio_fingerprints = list()
+    audio_fingerprints_info = list()
+    # extracting audio id
+    audio_id = i.split("/")[5].split(".")[0]
+    # reading time series audio data re-sampled at 7KHz
+    audio_data = AudioManager.load_audio(audio_path=i)
+    # computing spectrogram of the audio
+    spectrogram = stft.compute_stft_magnitude_in_db(audio_data=audio_data)
+    # extracting spectral peaks
+    spectral_peaks = peak_extractor.extract_spectral_peaks_2(spectrogram=spectrogram)
+    # generate fingerprints
+    fingerprint_generator.generate_fingerprints(spectral_peaks=spectral_peaks[0],
+                                                audio_fingerprints=audio_fingerprints,
+                                                audio_fingerprints_info=audio_fingerprints_info,
+                                                r=1.0,
+                                                c=4,
+                                                fixed=False,
+                                                no_groups=2)
+    raw_index = int(ConfigManager.read_config(config_file_path=config_file_path,
+                                              section="Default", sub_section="Raw_Index"))
+    fingerprint_index = 0
+    for j in audio_fingerprints:
+        row = [audio_id] + audio_fingerprints_info[fingerprint_index]
+        RTreeManager.insert_node(rtree_index=r_tree_index, node_id=raw_index, geo_hash=j)
+        RawDataManager.insert_data(shelf=shelf_index,key=raw_index,value=row)
+        raw_index += 1
+        fingerprint_index += 1
+    ConfigManager.write_config(config_file_path=config_file_path,
+                               section="Default",
+                               sub_section="Raw_Index",
+                               value=str(raw_index))
+    print("Done With Fingerprinting ", iter)
+    iter += 1
