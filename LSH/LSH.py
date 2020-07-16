53aa5d88d5446bf909502fd1e445253336b6a959
efode-r2d2 <efidesalew@gmail.com>
2020-07-16 21:07:03 +0300

    lsh modified using multiple hash table

---
+++ LSH/LSH.py
@@ -0,0 +1,38 @@
+"""
+    < Efode is an an open source audio fingerprinting system>
+    Copyright (C) <2019>  <Efriem Desalew Gebie>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from LSH import HashTable
+
+
+class LSH:
+    def __init__(self, num_tables, hash_size, inp_dimensions):
+        self.num_tables = num_tables
+        self.hash_size = hash_size
+        self.inp_dimensions = inp_dimensions
+        self.hash_tables = list()
+        for i in range(self.num_tables):
+            self.hash_tables.append(HashTable(self.hash_size, self.inp_dimensions))
+
+    def __setitem__(self, inp_vec, label):
+        for table in self.hash_tables:
+            table[inp_vec] = label
+
+    def __getitem__(self, inp_vec):
+        results = list()
+        for table in self.hash_tables:
+            results.extend(table[inp_vec])
+        return list(set(results))
