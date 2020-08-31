d3d2430e9b5e292b37d0e1a39780e25579502372
efode-r2d2 <efidesalew@gmail.com>
2020-08-31 23:28:58 +0300

    dir_manager refactored

--- Utilities/dir_manager.py
+++ Utilities/dir_manager.py
@@ -3,8 +3,14 @@ import os
 
 def find_mp3_files(src_dir):
     """
+    A function to list all mp3 files under a given source directory.
 
-    :type src_dir: object
+    Parameters:
+        src_dir (String): Source directory.
+
+    Returns:
+         List : List of paths for all mp3 files.
+
     """
     mp3_files = []
     for r, d, f in os.walk(src_dir):
@@ -16,8 +22,14 @@ def find_mp3_files(src_dir):
 
 def find_wav_files(src_dir):
     """
+    A function to list all wav files under a given source directory.
 
-    :type src_dir: object
+    Parameters:
+        src_dir (String): Absolute/Relative path of source directory.
+
+    Returns:
+        List : List of all wav files.
+
     """
     wav_files = []
     for r, d, f in os.walk(src_dir):
@@ -29,8 +41,11 @@ def find_wav_files(src_dir):
 
 def create_dir(src_dir):
     """
+    A function to create a directory given its path.
 
-    :type src_dir: object
+    Parameters:
+        src_dir (String): Directory path.
+
     """
     if not os.path.exists(src_dir):
         os.makedirs(src_dir)
